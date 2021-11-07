591ba09fa631afa1f0340c6b83943a1410b85892
hermitty <wiktoria.pustelniak@gmail.com>
2021-11-07 09:24:26 +0100

    Add test controller

---
+++ Api/RumLogger/RumLogger.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RumLogger.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public bool Test()
+        {
+            return true;
+        }
+    }
+}
